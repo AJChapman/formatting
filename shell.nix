6f5bc6fc15d76776d7b0e958d46fe8e506d57e3a
Alex Chapman <alex@farfromthere.net>
2020-10-09 21:20:27 +1100

    Properly add ghcid in shell.nix

--- shell.nix
+++ shell.nix
@@ -9,7 +9,7 @@ let
 
   drv = import ./default.nix { inherit pkgs compiler doBenchmark doTest; };
   drvWithTools = pkgs.haskell.lib.addBuildDepends drv (with pkgs; [
-    cabal-install haskellPackages.ghcid haskellPackages.pretty-simple haskellPackages.weeder haskellPackages.doctest
+    cabal-install ghcid haskellPackages.pretty-simple haskellPackages.weeder haskellPackages.doctest
   ]);
 in
   if pkgs.lib.inNixShell then drvWithTools.env else drv
